8e3edb2f5f2ac2845195031d547d281ce6aa084c
Minamin <minamin37345@gmail.com>
2022-10-15 16:16:20 +0900

    コメントの追加。

--- Classes/Language.cs
+++ Classes/Language.cs
@@ -6,14 +6,14 @@ namespace AutoCoder
     public partial class Language
     {
         public string? languageName { get; set;}  //言語名
-        public string s_include = "";
-        public string s_namespace = "";
-        public string s_block_begin = "";
-        public string s_block_end = "";
-        public string s_arg_begin = "";
-        public string s_arg_end = "";
-        public string s_line_end = "";
-        public string s_set = "";
+        public string s_include = "";             //ライブラリ等の追加キーワード
+        public string s_namespace = "";           //名前空間の記述キーワード
+        public string s_block_begin = "";         //処理記述ブロックの開始文字
+        public string s_block_end = "";           //処理記述ブロックの終了文字
+        public string s_arg_begin = "";           //引数記述の開始文字
+        public string s_arg_end = "";             //引数記述の終了文字
+        public string s_line_end = "";            //処理の行末文字
+        public string s_set = "";                 //代入文字
         public Language() {}
     };
 }

0b7a7fbe7a2cc879a1ea3ccebffddc5e39ed4464
Minamin <minamin37345@gmail.com>
2022-10-15 13:19:17 +0900

    クラスの追加。

---
+++ Classes/Enum.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace AutoCoder
+{
+    //列挙体の定義を格納しておくクラス。
+    public partial class Enum : Element
+    {
+        public Enum() { }
+    };
+}
---
+++ Classes/Struct.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace AutoCoder
+{
+    //構造体の定義を格納しておくクラス。
+    public partial class Struct : Element
+    {
+        public Struct() { }
+    };
+}

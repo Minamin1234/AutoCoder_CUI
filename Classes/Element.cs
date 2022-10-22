5ae77c11b8df499092e1efe0c30e9be2cfeaa04f
Minamin1234 <minamin37345@gmail.com>
2022-10-23 03:01:36 +0900

    保存

--- Classes/Element.cs
+++ Classes/Element.cs
@@ -5,9 +5,21 @@ namespace AutoCoder
     //ソースコードに記述する全ての要素
     public partial class Element
     {
+        public uint level = 1;                 //インデントレベル
         public string? name { get; set;}       //要素名
         public Language? language { get; set;} //記述する言語
+        public string? content { get; set;}    //ビルドされた要素(文字列)
         public Element() { }
+
+        public virtual void addToContent(string addstr)
+        {
+            this.content += (this.language.s_tab * this.level) + addstr;
+        }
+
+        public void operator<< (Element e1,string addstr)
+        {
+            el.content += (el.language.s_tab * el.level) + addstr;
+        }
 
         //ソースコードが生成される際に呼ばれます。
         public virtual string on_Build() { return "";}
--- Classes/Namespace.cs
+++ Classes/Namespace.cs
@@ -3,7 +3,7 @@ using System;
 namespace AutoCoder
 {
     //名前空間の定義を格納しておくクラス
-    public partial class Namespace : Element
+    public partial class Namespace : Element,INestable
     {
         public Namespace? fromNamespace { get; set;}   //親の名前空間
         public string? namespaceName { get{ return this.name;} set{ this.name = value;}}
@@ -11,7 +11,44 @@ namespace AutoCoder
         public Namespace() { }
         public override string on_Build()
         {
-            return this.language.Namespace(ref this);
+            string res = string.Empty;
+            this << string.Empty;
+            if (this.elements == null) return res;
+            this << this.language.s_namespace;
+            this << this.language.s_space;
+            this << this.namespaceName;
+            this << this.language.s_newline;
+            this << this.language.s_block_begin;
+            this << this.language.s_tab;
+            this << this.on_Build_childs();
+            this << this.language.s_newline;
+            this << this.language.s_block_end;
+            this << this.language.s_newline;
+
+            /*
+            res += this.language.s_namespace;
+            res += this.language.s_space;
+            res += this.namespaceName;
+            res += this.language.s_newline;
+            res += this.language.s_block_begin;
+            res += this.language.s_tab;
+            res += this.on_Build_childs();
+            res += this.language.s_newline;
+            res += this.language.s_block_end;
+            res += this.language.s_newline;
+            */
+            return res;
+        }
+
+        public string on_Build_childs()
+        {
+            string res = string.Empty;
+            if (this.elements == null) return res;
+            foreach(var e in this.elements)
+            {
+                res += e.on_Build();
+            }
+
         }
     };
 }

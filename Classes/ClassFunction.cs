191999c16c232c306e54cf9f7478020be944ef71
Minamin1234 <minamin37345@gmail.com>
2022-10-30 02:55:12 +0900

    メンバ関数要素の修正

--- Classes/ClassFunction.cs
+++ Classes/ClassFunction.cs
@@ -7,7 +7,6 @@ namespace AutoCoder
     {
         public Class memberOf = null;                              //関数を持つクラス
         public E_AccessLevel accessLevel = E_AccessLevel.PRIVATE;  //関数のアクセスレベル
-        public Type returnType = null;                             //関数の戻り値型
         public ClassFunction() { }
     };
 }
